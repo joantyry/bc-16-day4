20c5569437af55325509c4942dd0a467f9fbb9c7
joantyry <tyry7979@gmail.com>
2017-03-09 07:42:01 +0300

    first commit

--- MissingNumber.py
+++ MissingNumber.py
@@ -1,4 +1,11 @@
 def find_missing(a,b):
+  '''
+  You are presented with two arrays, all containing positive integers. One of the arrays will have one extra number, see below:
+
+[1,2,3] and [1,2,3,4] should return 4
+
+[4,66,7] and [66,77,7,4] should return 77
+'''
  
   a,b = set(a),set(b)
   if  (len(a) == len (b) or len(a) == 0 or len(b)==0):
